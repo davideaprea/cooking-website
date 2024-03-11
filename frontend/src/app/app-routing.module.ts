46b47172a3241656e09b4614712de777b5c6ace3
davideaprea <apredav.developer@gmail.com>
2024-03-11 12:09:40 +0100

    profile module creation

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { userLoggedGuard } from './auth/guards/user-logged.guard';
+import { userNotLoggedGuard } from './auth/guards/user-not-logged.guard';
 
 const routes: Routes = [
-  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule), canActivate: [userLoggedGuard] }
+  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule), canActivate: [userLoggedGuard] },
+  { path: 'profile', loadChildren: () => import('./features/profile/profile.module').then(m => m.ProfileModule), canActivate: [userNotLoggedGuard] }
 ];
 
 @NgModule({
